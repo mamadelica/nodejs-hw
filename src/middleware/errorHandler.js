bdda3a411b2b9008b609140603041392a42911a8
Anastasia Mamatova <mamatovaanastasia31@gmail.com>
2025-12-20 11:31:26 +0200

    fix mistakes

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,15 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
-  console.error('Error:', err.message);
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({ message: err.message || err.name });
+  }
+
+  const isProd = process.env.NODE_ENV === 'production';
+
   res.status(500).json({
-    message: 'Internal Server Error',
-    error: err.message,
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
   });
 };
